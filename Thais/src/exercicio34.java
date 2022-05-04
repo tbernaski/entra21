bbea78d685fbbae132e185302c4f48f2856b8597
Thaís Karoline Bernaski <tbernaski@furb.br>
2022-05-03 21:47:21 -0300

    hoje3

---
+++ Thais/src/exercicio34.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class exercicio34 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o número de palavras que deseja");
+        int numero = in.nextInt();
+
+        String[] palavras = new String[numero];
+        for (int i = 0; i < numero; i++) {
+            System.out.println("Informe a palavra");
+            palavras[i] = in.next();
+        }
+        String palavrafinal = "";
+        for (int i = 0; i < palavras.length; i++) {
+            if (i == 0) {
+                palavrafinal+=palavras[i];
+            } else {
+                palavrafinal+= ", " +palavras[i];
+            }
+        }
+        System.out.println(palavrafinal);
+    }
+}
+
