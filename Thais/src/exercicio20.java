5aa361beb2e7fc1731045e287b770244b6a7689b
Thaís Karoline Bernaski <tbernaski@furb.br>
2022-05-06 21:50:41 -0300

    today

--- Thais/src/exercicio20.java
+++ Thais/src/exercicio20.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
-public class exercicio20 {
-    public static void main(String[] args) {
+//public class exercicio20 {
+  //  public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Digite o nome :\n ");
-        String nome = in.next();
-    }
-}
+    //    Scanner in = new Scanner(System.in);
+      //  System.out.print("Digite uma frase :\n ");
+        //String frase = in.next();
+
+        //System.out.println(frase.length());
+        //System.out.println("A quantidade de caracteres é: ", frase);
+
+   // }/
+//}
---
+++ Thais/src/exerciciodiaseis.java
@@ -0,0 +1,6 @@
+//blic class exerciciodiaseis//
+   // public static void main(String[] args) {
+     //   int [] [] mapa = {
+       //         {0,0,0,0,0,},
+         //       {0,0,0,0,0,},
+           //     {0,0,0,0,0,},
