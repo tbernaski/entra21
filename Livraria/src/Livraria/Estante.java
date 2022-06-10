8e9f28d258de01c1080715e8b6defcaf621e94d3
Thaís Karoline Bernaski <tbernaski@furb.br>
2022-06-09 21:49:18 -0300

    dia09_06

--- Livraria/src/Livraria/Estante.java
+++ Livraria/src/Livraria/Estante.java
@@ -1,5 +1,7 @@
 package Livraria;
 
+import java.util.Locale;
+
 public class Estante {
     private int capMaxima;
     private Item[] itens;
@@ -12,36 +14,46 @@ public class Estante {
 
 
     public boolean estanteCheia() {
-       if (){
-    }
+       return this.quantidadeItens() == this.getCapMaxima();
     }
 
 
     public int quantidadeItens(){
-
-
-        return 0;
+        int contador = 0;
+        for (Item i: this.getItens()){
+            if (i !=null){
+                contador++;
+            }
+        }
+        return contador;
     }
 
-
     public Item buscarItem (String titulo){
-return null;
+        for (Item i : this.getItens()){
+           if (i != null && i.getTitulo().toLowerCase().contains(titulo.toLowerCase())) {
+               return i;
+           }
+        }
+    return null;
     }
 
 
     public boolean adicionarItem (Item item){
-
-
+    for (int i = 0; i < this.getItens().length; i++) {
+        if (this.getItens()[i] == null) {
+            this.getItens()[i] = item;
+            return true;
+        }
+        }
         return false;
     }
 
 
-    public Item removerItem (int posicao){
-return null;
-
+    public Item removerItem (int posicao) {
+        Item i = this.getItens()[posicao];
+        this.getItens()[posicao] = null;
+        return i;
     }
-
-
 
 
 
--- Livraria/src/Main.java
+++ Livraria/src/Main.java
@@ -1,3 +1,6 @@
+import Livraria.DVD;
+import Livraria.Estante;
+import Livraria.Item;
 import Livraria.Livro;
 
 import java.util.Scanner;
@@ -5,12 +8,97 @@ import java.util.Scanner;
 public class Main {
 
     public static Scanner in = new Scanner(System.in);
+    private static Livro escolha;
+    private static int Livro;
+
     public static void main(String[] args) {
-        Livro l = new Livro();
-        l.setTitulo("Harry Potter");
-        l.avaliar();
-        l.avaliar();
-        l.avaliar();
-        System.out.println(l.getTotalRating());
+
+        System.out.println("-MENU: Escolha um item para adicionar-");
+        System.out.println("(1) - Livro");
+        System.out.println("(2) - DVD");
+        int escolha = in.nextInt();
+        in.nextLine();
+        Item item = null;
+
+        switch (escolha) {
+            case 1:
+                item = new Livro();
+                break;
+            case 2:
+                item = new DVD();
+                break;
+        }
+
+        if (escolha == 1) {
+            System.out.println("Digite o nome do autor do livro: ");
+            in.nextLine();
+            System.out.println("Digite a quantidade de páginas do livro: ");
+            in.nextLine();
+            System.out.println("Digite o ano de publicação do livro: ");
+            in.nextLine();
+            System.out.println("Digite o edição do livro: ");
+            in.nextLine();
+                System.out.println("Deseja inserir mais um item? (S/N)");
+            if (in.nextLine().equalsIgnoreCase("N")) {
+                
+
+            } else {
+                System.out.println("Digite o nome do diretor do DVD: ");
+                in.nextLine();
+                System.out.println("Digite o tempo de duração do DVD: ");
+                in.nextLine();
+                System.out.println("Digite o ano de lançamento do DVD: ");
+                in.nextLine();
+                System.out.println("Deseja inserir mais um item? (S/N)");
+                if (in.nextLine().equalsIgnoreCase("N")) {
+
+                }
+            }
+        }
+
+
+
+
+            Estante l = new Estante(5);
+            l.getItens();
+            for (int i = 0; i < 5; i++) {
+                l.getItens();
+                if (i == 4) {
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            getLivro();
+            for (int j = 0; i < 5; i++) {
+            }
+        }
+    }
+
+
+    public static Scanner getIn() {
+        return in;
+    }
+
+    public static void setIn(Scanner in) {
+        Main.in = in;
+    }
+
+    public static Livraria.Livro getEscolha() {
+        return escolha;
+    }
+
+    public static void setEscolha(Livraria.Livro escolha) {
+        Main.escolha = escolha;
+    }
+
+    public static int getLivro() {
+        return Livro;
+    }
+
+    public static void setLivro(int livro) {
+        Livro = livro;
     }
 }
+
+
